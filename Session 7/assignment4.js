b35dd086c89e842ff517882ff1678a5257bfff8d
Varun Karanda <varunkaranda@gmail.com>
2022-02-11 14:00:37 +0530

    Changed

--- Session 7/assignment4.js
+++ Session 7/assignment4.js
@@ -3,11 +3,22 @@
 // Sample Input: [10, 20, 30, 10, 40, -2]
 // Sample Output: 20, 40, -2
 
-let array = [2, 4, 6, 2, 8, 6, 2, 8, 4, 8];
-let obj = {}
+let array = [2, 4, 6, 2, 8, 6, 2, 8, 4, 8, 9, 10];
+let obj = {};
+let newArray = [];
 
-for (let i = 0; i < array.length; i++){
-    obj[array[i]] = i
+for (let i = 0; i < array.length; i++) {
+  if (obj[array[i]]) {
+    obj[array[i]] = obj[array[i]] + 1;
+  } else {
+    obj[array[i]] = 1;
+  }
 }
 
-console.log(Object.keys(obj).join(", "));
+for (keys in obj) {
+  if (obj[keys] === 1) {
+    newArray.push(keys);
+  }
+}
+
+console.log(newArray.join(","));
